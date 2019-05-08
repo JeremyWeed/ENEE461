f0de8308819ae15d107c51471d140b2842f9134c
granthoover <grant2hoover2@gmail.com>
2019-05-08 13:01:45 -0400

    working

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import wiringpi
 from find_ball import FindBall
 from csv_writer import CSVWriter
 
-x_min = 126
-x_max = 484
+x_min = 14
+x_max = 388
 x_goal = (x_min + x_max) / 2
 
 s_max = 114
 s_min = 80
 s_mid = 94
 
-kp = .03
-ki = 0
-kd = .01
+kp = .05
+ki = 0.01
+kd = 0.008
 
 error = 0
 integral = 0
@@ -24,7 +24,7 @@ time_last_loop = time.time()
 last_error = 0
 
 fb = FindBall(0)
-csvw = CSVWriter(data.csv)
+csvw = CSVWriter("data.csv")
 
 # use 'GPIO naming'
 wiringpi.wiringPiSetupGpio()
@@ -47,13 +47,13 @@ while True:
     time_this_loop = time.time()
     error = x_goal - x_ball
     integral += (time_this_loop - time_last_loop) * error
-    deriv = error - last_error
+    deriv = (error - last_error) / (time_this_loop - time_last_loop)
+    print(time_this_loop - time_last_loop)
     time_last_loop = time_this_loop
     print('err: {}'.format(error))
     print('integral: {}'.format(integral))
     print('deriv: {}'.format(deriv))
     print('x_ball: {}'.format(x_ball))
-    print(time_this_loop - time_last_loop)
     val = s_mid + kp*error + ki*integral + kd*deriv
     val = min(val, s_max)
     val = max(val, s_min)
